b37d85fd01c81f5bed565dbb5e91606162c3cb5e
Nicolas Gualteros <ngualterosp@unbosuqe.edu.co>
2020-04-16 12:51:30 -0500

    ini

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Caniscatus</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Me &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Arbutus+Slab|Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <body>
+    
+
+      <div class="site-wrap">
+
+        <div class="site-mobile-menu site-navbar-target">
+          <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+              <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div class="site-mobile-menu-body"></div>
+        </div>
+       
+        <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+    
+          <div class="container-fluid">
+            <div class="row align-items-center justify-content-center">
+              
+              <div class="">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+                  <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                    <li><a href="#home-section" class="nav-link">INICIO</a></li>
+                    <li><a href="#services-section" class="nav-link">NOSOTROS</a></li>
+                    <li><a href="#about-section" class="nav-link">NUESTROS <br> SERVICIOS</a></li>
+                    
+                  </ul>
+                </nav>
+              </div>
+    
+              <div class=" text-center mx-5">
+                <h1 class="m-0 site-logo"><a href="index.html">lOGO</a></h1>
+              </div>
+    
+              <div class="text-left">
+    
+                <nav class="site-navigation position-relative" role="navigation">
+                  <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                    <li><a href="#portfolio-section" class="nav-link">GALERIA</a></li>
+                    <li><a href="#clients-section" class="nav-link">CONTACTENOS</a></li>
+                
+                  </ul>
+                </nav>
+    
+    
+                <div class="d-inline-block d-lg-none" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+    
+              </div>
+    
+            </div>
+          </div>
+          
+        </header>
+    
+
+        @yield('content')
+
+    
+
+
+
+        <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+        <script src="{{asset('js/jquery-ui.js')}}"></script>
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+        <script src="{{asset('js/aos.js')}}"></script>
+        <script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
+        <script src="{{asset('js/jquery.sticky.js')}}"></script>
+        <script src="{{asset('js/isotope.pkgd.min.js')}}"></script>
+        <script src="{{asset('js/main.js')}}"></script>
+
+    </body>
+    </html>
